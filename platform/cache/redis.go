970b97df29cbe4ae9c32678723722465da05a3fc
Khalid <khalidalhabibie07@gmail.com>
2021-12-25 19:31:10 +0700

    disbale redis

--- platform/cache/redis.go
+++ platform/cache/redis.go
@@ -1,31 +1,27 @@
 package cache
 
 import (
-	"os"
-	"strconv"
-
-	"github.com/thena/pkg/utils"
-
 	"github.com/go-redis/redis/v8"
 )
 
 // RedisConnection func for connect to Redis server.
 func RedisConnection() (*redis.Client, error) {
-	// Define Redis database number.
-	dbNumber, _ := strconv.Atoi(os.Getenv("REDIS_DB_NUMBER"))
+	// // Define Redis database number.
+	// dbNumber, _ := strconv.Atoi(os.Getenv("REDIS_DB_NUMBER"))
 
-	// Build Redis connection URL.
-	redisConnURL, err := utils.ConnectionURLBuilder("redis")
-	if err != nil {
-		return nil, err
-	}
+	// // Build Redis connection URL.
+	// redisConnURL, err := utils.ConnectionURLBuilder("redis")
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	// Set Redis options.
-	options := &redis.Options{
-		Addr:     redisConnURL,
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       dbNumber,
-	}
+	// // Set Redis options.
+	// options := &redis.Options{
+	// 	Addr:     redisConnURL,
+	// 	Password: os.Getenv("REDIS_PASSWORD"),
+	// 	DB:       dbNumber,
+	// }
 
-	return redis.NewClient(options), nil
+	// return redis.NewClient(options), nil
+	return nil, nil
 }
